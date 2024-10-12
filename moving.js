7a63916a00fe30528f75d3fb810d0c7b5a38af77
SalvisAttiķis <salvis.attikis@gmail.com>
2024-10-12 17:29:42 +0300

    added movement and structure for about page next is styling and finishing

--- moving.js
+++ moving.js
@@ -4,14 +4,28 @@ function navigateToPage(url) {
 }
 
 // Add event listeners to each <ul> item
+document.getElementById('home').addEventListener('click', function() {
+    navigateToPage('index.html');
+});
 document.getElementById('projects').addEventListener('click', function() {
     navigateToPage('projects.html');
 });
-
 document.getElementById('about').addEventListener('click', function() {
     navigateToPage('about.html');
 });
-
 document.getElementById('resume').addEventListener('click', function() {
     navigateToPage('https://docs.google.com/document/d/1G5XeAc6qpj8SJD6b6GCyk7OgsJzjIfjiCJDHRRdSDjc/edit?usp=sharing');
+});
+document.getElementById('about1').addEventListener('click', function() {
+    navigateToPage('about.html');
+});
+
+document.getElementById('projects1').addEventListener('click', function() {
+    navigateToPage('projects.html');
+});
+document.getElementById('projects2').addEventListener('click', function() {
+    navigateToPage('projects.html');
+});
+document.getElementById('projects3').addEventListener('click', function() {
+    navigateToPage('projects.html');
 });
